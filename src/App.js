cf32aa8dd4b5688b628a7606bdaf2eb1e932a86c
johnnyferreiradev <johnnyferreiradevweb@gmail.com>
2020-04-29 10:40:15 -0300

    Style of the header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
+import './general-styles.css';
+import './App.css';
+
 function App() {
   return (
     <div className="App">
-      Hello World!
+      <header>
+        <h2>Play<span>&#38;</span>Tag</h2>
+      </header>
+      <main>
+        Content
+      </main>
+      <footer>
+        Desenvolvido por Johnny Ferreira
+      </footer>
     </div>
   );
 }
