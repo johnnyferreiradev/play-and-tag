6f3ef903e0cee22ac6c578a879c72ca401be4c63
nicolas433 <ngrisoste@gmail.com>
2020-05-05 09:27:11 -0300

    contexte menu on video

--- src/components/VideoPlayer/index.jsx
+++ src/components/VideoPlayer/index.jsx
@@ -1,16 +1,35 @@
-import React from 'react';
-
-import './styles.css';
+import React from "react";
+import { Player } from "video-react";
+import "./styles.css";
+import { ContextMenu, MenuItem, ContextMenuTrigger } from "react-contextmenu";
+import "../../../node_modules/video-react/dist/video-react.css";
 
 export default function VideoPlayer() {
+  function handleComment() {
+    console.log("ok");
+  }
+
   return (
     <div className="video-player">
-        <div className="video-area">
-            <video width="1080px" controls>
-                <source src="https://www.w3schools.com/html/mov_bbb.mp4" type="video/mp4" />
-                teste
-            </video>
+      <ContextMenuTrigger id="some_unique_identifier" className="context-menu">
+        <div className="testes">
+          <Player
+            playsInline
+            poster="/assets/poster.png"
+            src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
+          ></Player>
         </div>
+      </ContextMenuTrigger>
+
+      <ContextMenu id="some_unique_identifier">
+        <MenuItem
+          className="context-menu-item"
+          data={{ foo: "bar" }}
+          onClick={handleComment}
+        >
+          Adicionar comentário
+        </MenuItem>
+      </ContextMenu>
     </div>
   );
 }
